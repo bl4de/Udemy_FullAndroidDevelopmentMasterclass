a29cf4023c59605f4fa71c34196324ab920a6956
Rafal Janicki <bloorq@gmail.com>
2023-01-01 20:44:32 +0000

    Some TextView and Button stuff

--- FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
+++ FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.firstproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textMain;
+    Button btnOne;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textMain = findViewById(R.id.textMain);
+        textMain.setText("Hello First App!");
+
+        btnOne = findViewById(R.id.btnOne);
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textMain.setText("Uh oh, " + btnOne.getId() + " was clicked!");
+            }
+        });
     }
 }
