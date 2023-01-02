fbb6b3434a37a703eed3ec0b358f686de769d6a4
Rafal Janicki <bloorq@gmail.com>
2023-01-02 20:48:22 +0000

    ImageView  example

--- FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
+++ FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.firstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textMain;
     Button btnOne;
     EditText editText;
+    ImageView neuromancer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editText = findViewById(R.id.editTextTextPersonName);
+        neuromancer = findViewById(R.id.neuromancer);
         textMain = findViewById(R.id.textMain);
         textMain.setText("Hello First App!");
 
@@ -27,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         btnOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textMain.setText(editText.getText());
+                textMain.setText(editText.getText().toString());
+                neuromancer.setVisibility(View.INVISIBLE);
             }
         });
     }
