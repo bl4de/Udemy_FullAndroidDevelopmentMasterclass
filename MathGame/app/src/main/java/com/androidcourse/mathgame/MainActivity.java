0d491b4fa16090e3f4ffb4173e2bb5e616155a3c
Rafal Janicki <bloorq@gmail.com>
2023-02-27 15:04:49 +0000

    Android Project 2 - MathGame - in progress

---
+++ MathGame/app/src/main/java/com/androidcourse/mathgame/MainActivity.java
@@ -0,0 +1,33 @@
+package com.androidcourse.mathgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button addition;
+    Button substraction;
+    Button multi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addition = findViewById(R.id.buttonAdd);
+        substraction = findViewById(R.id.buttonSub);
+        multi = findViewById(R.id.buttonMulti);
+
+        addition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Game.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
