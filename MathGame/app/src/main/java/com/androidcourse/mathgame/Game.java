8d418e46b0470680de95acbd93f146c571e15c28
Rafal Janicki <bloorq@gmail.com>
2023-03-04 08:10:19 +0000

    Android Project 2 - MathGame - in progress

--- MathGame/app/src/main/java/com/androidcourse/mathgame/Game.java
+++ MathGame/app/src/main/java/com/androidcourse/mathgame/Game.java
@@ -3,11 +3,13 @@ package com.androidcourse.mathgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
 import java.util.Random;
 
 public class Game extends AppCompatActivity {
@@ -31,6 +33,11 @@ public class Game extends AppCompatActivity {
     int userScore = 0;
     int userLife = 3;
 
+    CountDownTimer timer;
+    private static final long START_TIMER_IN_MILIS = 10000;
+    Boolean timer_running;
+    long time_left_in_milis = START_TIMER_IN_MILIS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,13 +59,13 @@ public class Game extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 userAnswer = Integer.valueOf(answer.getText().toString());
+                pauseTimer();
                 if (userAnswer == realAnswer) {
                     userScore = userScore + 10;
                     question.setText("Congratulations! Your answer is correct!");
                     score.setText("" + userScore);
                 } else {
-                    userLife = userLife - 1;
-                    life.setText("" + userLife);
+                    decreaseLifes();
                     question.setText("Sorry, your answer is incorrect :(");
                 }
             }
@@ -67,6 +74,8 @@ public class Game extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                answer.setText("");
+                resetTimer();
                 gameContinue();
             }
         });
@@ -75,8 +84,52 @@ public class Game extends AppCompatActivity {
     public void gameContinue() {
         number1 = random.nextInt(100);
         number2 = random.nextInt(100);
-        answer.setText("");
         realAnswer = number1 + number2;
         question.setText(number1 + " + " + number2);
+        startTimer();
+    }
+
+    public void startTimer() {
+        timer = new CountDownTimer(time_left_in_milis, 1000) {
+            @Override
+            public void onTick(long l) {
+                time_left_in_milis = l;
+                updateText();
+            }
+
+            @Override
+            public void onFinish() {
+                timer_running = false;
+                pauseTimer();
+                resetTimer();
+                updateText();
+                decreaseLifes();
+                question.setText("Sorry, time is up... :(");
+            }
+        }.start();
+
+        timer_running = true;
+    }
+
+    public void updateText() {
+        int second = (int)(time_left_in_milis / 1000) % 60;
+        String time_left = String.format(Locale.getDefault(), "%02d", second);
+        time.setText(time_left);
+    }
+
+    public void pauseTimer() {
+        timer.cancel();
+        timer_running = false;
+    }
+
+    public void resetTimer() {
+        time_left_in_milis = START_TIMER_IN_MILIS;
+        updateText();
+    }
+
+    // MY METHODS
+    public void decreaseLifes() {
+        userLife = userLife - 1;
+        life.setText("" + userLife);
     }
 }
