b8aeaa476fa096f9301d0b567420132c4480b3c1
Rafal Janicki <bloorq@gmail.com>
2023-03-11 13:33:14 +0000

    Android Project 3 - Calculator - in progress

--- Calculator/app/src/main/java/com/androidcourse/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/androidcourse/calculator/MainActivity.java
@@ -3,12 +3,125 @@ package com.androidcourse.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAc, btnDel, btnDivide, btnMulti, btnMinus, btnPlus, btnEquals, btnDot,
+            btn9, btn8, btn7, btn6, btn5, btn4, btn3, btn2, btn1, btn0;
+
+    private TextView textViewHistory, textViewResult;
+
+
+    private String number = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+
+        btnAc = findViewById(R.id.btnAc);
+        btnDel = findViewById(R.id.btnDel);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnMulti = findViewById(R.id.btnMulti);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnEquals = findViewById(R.id.btnEquals);
+        btnDot = findViewById(R.id.btnDot);
+
+        textViewHistory = findViewById(R.id.textViewHistory);
+        textViewResult = findViewById(R.id.textViewResult);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("0");
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("1");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("2");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("3");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("4");
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("5");
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("6");
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("7");
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("8");
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("9");
+            }
+        });
+    }
+
+    public void numberClick(String view) {
+        if (number == null) {
+            number = view;
+        } else {
+            number = number + view;
+        }
+        textViewResult.setText(number);
     }
 }
